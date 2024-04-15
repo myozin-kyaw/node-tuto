830cf1a8c69ab8eadb5d161b5a88405d98928f62
Myo Zin Kyaw <108007637+myozin-kyaw@users.noreply.github.com>
2024-04-15 08:48:36 +0630

    test async and syc

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const _ = require('lodash');
+const http = require('http');
 
-const myarray = [1, 2, [3, 4], [5, [6, 7, [8, 9]]]];
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.end('Welcome to our first node.js project.');
+    } else if (req.url == '/about') {
+        res.end(`
+            <h1>About Page</h1>
+            <p>This is about section</p>
+            <a href="/">Move back to home page</a>
+        `);
+    } else {
+        res.end(`
+            <h1>404 Not Found</h1>
+            <p>This page doesn't exist</p>
+            <a href="/">Move back to home page</a>
+        `);
+    }
+});
 
-console.log(_.flattenDeep(myarray))
+server.listen(2002, () => {
+    console.log('Server is running in port 2002');
+});
